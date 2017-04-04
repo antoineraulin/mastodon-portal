49867c91bb3f828124fd4efc101a29a873b25f17
antoineraulin <antoin.raulin@icloud.com>
2017-04-05 00:24:43 +0200

    fix

--- index.php
+++ index.php
@@ -44,6 +44,7 @@ error_reporting(-1);
                 $time = false;
             }else{
                 $content = explode("    ", $tr);
+                echo $tr;
                 echo "  url :'".$content[1]."'  ";
                 echo "  state :'".$content[0]."'  ";
                 echo "  registration :'".$content[3]."'  ";
