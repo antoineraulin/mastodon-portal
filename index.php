bd0ba6ec84f48720f7842381864ab9cba11523b2
antoineraulin <antoin.raulin@icloud.com>
2017-04-07 22:56:35 +0200

    fix

--- index.php
+++ index.php
@@ -33,6 +33,9 @@
   width: 15em;
   border-radius: 7px;
 }
+.img-circle {
+    border-radius: 50%;
+}
         </style>
 
 </head>
@@ -129,7 +132,7 @@ Mastodon Portal
     <?php
     switch ($lang){
     case "fr":
-        echo "<p>Mastodon est un réseau social <em>gratuit et open source</em>. Une alternative <em>décentralisée</em> aux plates-formes commerciales, elle évite les risques d'une seule société qui monopolise vos communications. Choisissez un serveur dont vous avez confiance. Selon votre choix , vous pouvez interagir avec tous les autres ou non. N'importe qui peut exécuter sa propre instance de Mastodon et participer au <em>réseau social</em> de façon transparente.<br>Sur mastodon un serveur s'appelle une instance<br>Mastodon Portal vérifie que l'instance qui vous est proposée est accessible et qu'il est possible d'y créer un compte.<br>Lorsque vous cliquerez sur <em>Commencer</em> vous serez redirigé directement sur l'instance choisie.<br>Mastodon Portal vous propose l'instance ".$instancechoosed.".<br>Si pour vous, le fonctionnement de ce réseau social est encore un mystère, Nous vous invitons à lire l'article de <a style=\"text-decoration: none;\" href=\"http://numerama.com\" >Numerama </a> en cliquant <a id=\"num\" data-tooltip=\"#numerama\" style=\"text-decoration: none;\" href=\"http://www.numerama.com/tech/246684-debuter-sur-mastodon-9-questions-pour-tout-comprendre-au-reseau-social-decentralise.html\">ici</a></p>.";
+        echo "<p>Mastodon est un réseau social <em>gratuit et open source</em>. Une alternative <em>décentralisée</em> aux plates-formes commerciales, elle évite les risques d'une seule société qui monopolise vos communications. Choisissez un serveur dont vous avez confiance. Selon votre choix , vous pouvez interagir avec tous les autres ou non. N'importe qui peut exécuter sa propre instance de Mastodon et participer au <em>réseau social</em> de façon transparente.<br>Sur mastodon un serveur s'appelle une instance<br>Mastodon Portal vérifie que l'instance qui vous est proposée est accessible et qu'il est possible d'y créer un compte.<br>Lorsque vous cliquerez sur <em>Commencer</em> vous serez redirigé directement sur l'instance choisie.<br>Mastodon Portal vous propose l'instance ".$instancechoosed.".<br>Si pour vous, le fonctionnement de ce réseau social est encore un mystère, Nous vous invitons à lire l'article de <a style=\"text-decoration: none;\" href=\"http://numerama.com\" id=\"num\" data-tooltip=\"#numerama\">Numerama </a> en cliquant <a style=\"text-decoration: none;\" href=\"http://www.numerama.com/tech/246684-debuter-sur-mastodon-9-questions-pour-tout-comprendre-au-reseau-social-decentralise.html\">ici</a></p>.";
         break;
     case "it":
         echo "<p>Mastodon è un social networking <em>libero e open source</em>. Un <em>decentrata</em> alternativa alle piattaforme commerciali, evita il rischio di una singola azienda che monopolizza le vostre comunicazioni. Scegli un server di fiducia. A seconda della scelta, è possibile interagire con tutti gli altri oppure no. Chiunque può eseguire la propria istanza di Mastodon e partecipare nella <em>rete sociale</em> senza soluzione di continuità.<br>Mastodon su un server si chiama un'istanza<br>Mastodon Portal controlla che un'istanza che viene offerto è disponibile ed è possibile creare un account.<br>Quando si fa clic <em>Cominciare</em>, sarai portato direttamente all'istanza selezionata.<br>Mastodon portal propone l'istanza ".$instancechoosed." .</p>";
@@ -449,9 +452,9 @@ Mastodon Portal
 </div>
 <div class="diva" id="foo">
 <center>
-<img src="https://files.mastodon.social/accounts/avatars/000/038/053/original/c20a9da88cbf5459.png?1491298071" alt="C20a9da88cbf5459">
+<img class="img-circle" src="https://files.mastodon.social/accounts/avatars/000/038/053/original/c20a9da88cbf5459.png?1491298071" alt="C20a9da88cbf5459">
 <br>
-<b style="color: white;font-family: Arial;">Antoine Raulin</b>
+<b style="color: white;font-family: Arial;">Antoine Raulin</b><br>
 <b style="color: white;font-family: Arial;">@antoineraulin</b>
 
 <p>Développeur Web et Android. Accros aux nouvelles technos. <a href="https://mastodon.social/tags/materialdesign" class="mention hashtag">#<span>MaterialDesign</span></a>
@@ -460,11 +463,11 @@ Mastodon Portal
 </center>
 </div>
 <div class="diva" id="numerama">
-<center>
-<img src="https://social.numerama.com/system/accounts/avatars/000/000/006/original/533d33f50876c95e.jpg?1491464361" alt="C20a9da88cbf5459">
+<center><br>
+<img class="img-circle" src="https://social.numerama.com/system/accounts/avatars/000/000/006/original/533d33f50876c95e.jpg?1491464361" alt="C20a9da88cbf5459">
 <br>
-<b style="color: white;font-family: Arial;">Numerama</b>
-<b style="color: white;font-family: Arial;">@numerama</b>
+<b style="color: white;font-family: Arial;">Numerama</b><br>
+<b style="color: white;font-family: Arial;">@numerama</b><br>
 
 <p>Le média de référence sur l'innovation technologique et la société numérique www.numerama.com</p>
 </center>
